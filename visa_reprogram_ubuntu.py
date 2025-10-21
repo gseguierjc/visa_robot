c708cbe0b03b8b9d0044f04d96957d80ac4ce1af
Jean <gseguierjc@gmail.com>
2025-10-21 12:13:29 -0500

    quitando validacion de pagina

--- visa_reprogram_ubuntu.py
+++ visa_reprogram_ubuntu.py
@@ -148,7 +148,7 @@ def reprogram_appointment():
         driver.get(LOGIN_URL)
         time.sleep(8)  # espera breve para carga inicial
         print("URL actual:",driver.current_url)
-        print("Html parcial:",driver.page_source[:2000])  # depuración
+        # print("Html parcial:",driver.page_source[:2000])  # depuración
         # Paso 2: introducir credenciales
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "form")))
         driver.find_element(By.CSS_SELECTOR, "input[type='email']").send_keys(USERNAME)
